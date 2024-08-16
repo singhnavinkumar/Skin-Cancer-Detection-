10f3d75c8e924fb878ba6b6b7039ece430c9b660
Navin@8108 <geek1234@xyz.in>
2024-08-17 00:19:35 +0530

    first commit

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+import time
+time.clock = time.time
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+english_bot = ChatBot('Bot',
+                storage_adapter='chatterbot.storage.SQLStorageAdapter',
+                logic_adapters=[{'import_path': 'chatterbot.logic.BestMatch'},  
+                                ], trainer='chatterbot.trainers.ListTrainer')
+english_bot.set_trainer(ListTrainer)
+english_bot.initialize
+
+print("Bot : Hi There! What is your name?")
+
+while(True):
+    userText = input("user : ")
+    response = str(english_bot.get_response(userText))
+    print("Bot : " + response[2:])
+    if(userText == "exit" or userText == "Exit" or userText == "Bye" or userText == "bye"):
+        break
